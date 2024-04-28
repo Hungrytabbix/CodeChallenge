c618c8c1b5b32872d5bdf8f8bd90a6e10f400177
Hungrytabbix <bennasu121@gmail.com>
2024-04-28 02:40:45 -0400

    new update on network.tf

--- network.tf
+++ network.tf
@@ -44,12 +44,12 @@ resource "aws_security_group" "ec2" {
     from_port   = 22
     to_port     = 22
     protocol    = "TCP"
-    cidr_blocks = ["10.0.4.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -67,8 +67,8 @@ resource "aws_security_group" "alb" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
